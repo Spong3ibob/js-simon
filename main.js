591dc5bbec029fe357008fbd1c9adc1b5870a82b
Spong3ibob <leonardomaran17@gmail.com>
2022-11-07 10:40:28 +0100

    Aggiunto timer

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 const numberRandom = [];
+const timer = 3
 let i = 1;
 const container = document.querySelector(".number-stored")
 while(numberRandom.length <= 5){
-    var randInt = Math.floor(Math.random()*10);
+    var randInt = Math.floor(Math.random()*100);
     
     if(numberRandom.includes(randInt) === false){
         numberRandom.push(randInt);
     }
     i++;
 }
+const number = document.createElement("div");
+number.append(numberRandom)
+container.append(number)
 
-setTimeout(timeNumber, 3000)
+setTimeout(function(){
+    number.remove()
+},timer * 1000);
+
+setTimeout(timeNumber, 4000)
 function timeNumber(){
-    const number = document.createElement("div");
-    number.append(numberRandom)
-    container.append(number)
+    const numberGessed = [];
+ for(let i = 1; i <= 5; i++){
+    const numberInsert = prompt("Inserisci un numero");
+    if(numberInsert.includes(numberRandom)){
+        numberGessed.push(numberRandom);
+    }
+ }
+ console.log(numberGessed);
 }
 
 console.log(numberRandom);
